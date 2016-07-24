c59fb928c18d11e941999962da5aa641f8d6438b
Tigor Mangatur Manurung <tigormanurung@Tigors-MacBook-Pro.local>
2016-07-24 23:31:33 +0700

    - cam server init

---
+++ src/camserver/camsvr_module/camsvr_module.ino
@@ -0,0 +1,18 @@
+#include <ESP8266wifi.h>
+
+/*
+ * Camera Server Module
+ * ---------------------
+ * 
+ * this module used for car, with camera + httpServer
+ * 
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
